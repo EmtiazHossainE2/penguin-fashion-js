930459df366ac2338ded0f1f8aac73f56ee49735
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-10 03:44:43 +0600

    solve some problems

---
+++ Panda/script.js
@@ -0,0 +1,41 @@
+1//
+console.log('its working');
+2//
+const totalh2 = document.getElementsByTagName('h2')
+for (const h2 of totalh2) {
+    h2.style.color = 'lightblue'
+}
+3//
+const backpack = document.getElementById('backpack');
+backpack.style.backgroundColor = 'tomato';
+4//
+const cards = document.getElementsByClassName('card')
+for (const card of cards) {
+    card.style.borderRadius = '30px'
+}
+5//
+// const buyBtn = document.getElementsByClassName('panda-btn-buy-now');
+// onclick = buyNow;
+// function buyNow() {
+//     console.log('buy now clicked ')
+// }
+
+//function call kore 
+function onClick() {
+    console.log('Buy now clicked')
+}
+
+// direct use kore 
+document.getElementById('buy-btn').addEventListener('click', function () {
+    console.log('clicked')
+})
+
+6// 
+const buttons = document.getElementsByClassName('panda-btn-buy-now');
+for (const button of buttons) {
+    button.addEventListener('click', function (event) {
+        event.target.parentNode.removeChild(event.target)
+    })
+}
+
+
---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('hi');
+console.log('its working ')
+
+const header1 = document.getElementsByTagName('h1');
+for (const h1 of header1) {
+    h1.style.color = '#30cf14';
+}
